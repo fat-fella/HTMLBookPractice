63440ac9e477543438b076d68c9b779b9105957f
fat-fella <aznclee711@gmail.com>
2023-06-27 19:37:15 +0900

    수업

---
+++ html/resources/js/test.js
@@ -0,0 +1,14 @@
+// script 태그 작성하지 않음.
+// 팝업 창 종류 
+//알러트 - 확인
+window.alert("this is alert")
+//컴펌 - 확인/취소 (예/아니오)
+var confirmvalue = confirm("this is confirm")
+alert(confirmvalue);
+console.log(confirmvalue)
+document.write(confirmvalue);
+//프롬프트 - 입력 -확인/취소 (입력값 리턴됨 (true/false))
+var promptvlaue = prompt("this is prompt")
+alert(promptvlaue);
+console.log(promptvlaue);
+document.write(promptvlaue);
